5d6b15b8ecf071aa8ecca3dde5202ef4d2329cf4
clarkbains <clarkbains@gmail.com>
2020-12-28 20:19:49 -0500

    Added other unit to in game message times
    Signed-off-by: clarkbains <clarkbains@gmail.com>

--- connect4/src/ui/views/partials/msg.js
+++ connect4/src/ui/views/partials/msg.js
@@ -3,32 +3,38 @@ import { Gateway } from '../../gateway'
 
 export class Msg {
 
-    activate(o) {
-      this.msg = o.msg
-      this.col = o.col
+  activate(o) {
+    this.msg = o.msg
+    this.col = o.col
+    this.time = this.updateTime(this.msg.time)
+    setInterval(() => {
       this.time = this.updateTime(this.msg.time)
-      setInterval(()=>{
-        this.time = this.updateTime(this.msg.time)
-      }, 5000)
+    }, 5000)
+  }
+  updateTime(t) {
+    let curr = new Date()
+    if (t) {
+      let show = ((curr - t) / 1000)
+      let suffix = ""
+      if (show < 60) {
+        suffix = "s"
+      }
+      else if (show < 3600) {
+        show = show / 60
+        suffix = "m"
+      } else if (show < (3600 * 24)) {
+        show = show / 3600
+        suffix = "h"
+      }
+      else if (show < (3600 * 24 * 7)) {
+        show = show / (3600 * 24)
+        suffix = "d"
+      } else {
+        show = show / (3600 * 24 * 7)
+        suffix = "w"
+      }
+      return show.toFixed(0) + suffix
     }
-    updateTime(t) {
-      let curr = new Date()
-      if (t) {
-          let show = ((curr - t) / 1000)
-          let suffix = ""
-          if (show < 60) {
-              suffix = "s"
-          }
-          else if (show < 3060) {
-              show = show / 60
-              suffix = "m"
-          }
-          else {
-              show = show / 3600
-              suffix = "h"
-          }
-          return show.toFixed(0) + suffix
-      } 
-      return ""
+    return ""
   }
 }
